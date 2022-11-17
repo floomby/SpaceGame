291572c4be5d250c39743882264d9c0763e37d18
Josh Hoover <caboodlennm@gmail.com>
2022-11-17 12:00:11 -0700

    Bezier curves look better

--- src/dialogs/manufacturing.ts
+++ src/dialogs/manufacturing.ts
@@ -119,10 +119,27 @@ const bindManufacturingUpdaters = () => {
   // bindPostUpdater("inventory", populateManufacturingTable);
 };
 
+const drawConnectionSpline = (svg: SVGElement, x1: number, y1: number, x2: number, y2: number, highlight: boolean) => {
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", `M ${x1} ${y1} C ${x1} ${(y1 + y2) / 2} ${x2} ${(y1 + y2) / 2} ${x2} ${y2}`);
+  path.setAttribute("stroke", highlight ? "green" : "black");
+  path.setAttribute("stroke-width", "1");
+  path.setAttribute("fill", "none");
+  svg.appendChild(path);
+};
+
 const manufacturingBay = () => {
   return horizontalCenter([
     "<h2>Manufacturing Bay</h2>",
     `<div class="manufacturing"><svg id="manufacturingTree" height="1000" width="2000">
+  <style>
+    rect {
+      cursor: pointer;
+    }
+    text {
+      cursor: pointer;
+    }
+  </style>
   <g>
     <rect x="0" y="0" width="2000" height="1000" fill="#cccccccc" stroke="black" stroke-width="1" />
   </g>
@@ -133,8 +150,11 @@ const manufacturingBay = () => {
   ]);
 };
 
+let selectedRecipe: RecipeDag = undefined
+
 const drawDag = () => {
   const horizontalSpacing = 200;
+  const verticalSpacing = 130;
   const ns = "http://www.w3.org/2000/svg";
 
   const manufacturingTree = document.getElementById("manufacturingTree") as unknown as SVGSVGElement;
@@ -148,6 +168,34 @@ const drawDag = () => {
     const marginX = 100;
     const marginY = 0;
     const centerX = manufacturingTree.clientWidth / 2;
+
+    const drawEdges = (recipe: RecipeDag, highlight = false) => {
+      if (recipeDagRoot === recipe) {
+        for (const child of recipe.below) {
+          drawEdges(child);
+        }
+        return;
+      }
+      if (drawnEdges.has(recipe)) {
+        return;
+      }
+      drawnEdges.add(recipe);
+      const { x: x1, y: y1 } = coordinates.get(recipe)!;
+      if (recipe === selectedRecipe) {
+        highlight = true;
+      }
+      for (const child of recipe.below) {
+        const { x: x2, y: y2 } = coordinates.get(child)!;
+        drawConnectionSpline(edgeGroup, x1 + 50, y1 + 50, x2 + 50, y2, highlight);
+        drawEdges(child, highlight);
+      }
+    };
+
+    const redrawEdges = () => {
+      edgeGroup.innerHTML = "";
+      drawnEdges.clear();
+      drawEdges(recipeDagRoot);
+    };
 
     const drawSvgElements = (recipe: RecipeDag) => {
       if (recipeDagRoot === recipe) {
@@ -163,7 +211,7 @@ const drawDag = () => {
       const level = recipe.drawLevel;
       const levelOffset = (recipesPerLevel[level] * horizontalSpacing + 100) / 2;
       const x = drawnPerLevel[level] * horizontalSpacing + marginX + centerX - levelOffset;
-      const y = (recipeDagRoot.minLevel + 1 - level) * 100 + marginY;
+      const y = (recipeDagRoot.minLevel + 1 - level) * verticalSpacing + marginY;
       console.log("drawing", recipe.recipe?.name, "at", x, y, drawnPerLevel[level]);
       coordinates.set(recipe, { x, y });
       drawnPerLevel[level] += 1;
@@ -184,6 +232,21 @@ const drawDag = () => {
       text.setAttribute("font-size", "12");
       text.innerHTML = recipe.recipe?.name || "Root";
       nodeGroup.appendChild(text);
+
+      const clickHandler = (e) => {
+        console.log("clicked", recipe.recipe?.name);
+      }
+
+      rect.addEventListener("click", clickHandler);
+      text.addEventListener("click", clickHandler);
+
+      const highlightHandler = (e) => {
+        selectedRecipe = recipe;
+        redrawEdges();
+      };
+      rect.addEventListener("mouseover", highlightHandler);
+      text.addEventListener("mouseover", highlightHandler);
+
       for (const child of recipe.below) {
         drawSvgElements(child);
       }
@@ -193,32 +256,6 @@ const drawDag = () => {
 
     const edgeGroup = document.createElementNS(ns, "g");
     const drawnEdges = new Set<RecipeDag>();
-
-    const drawEdges = (recipe: RecipeDag) => {
-      if (recipeDagRoot === recipe) {
-        for (const child of recipe.below) {
-          drawEdges(child);
-        }
-        return;
-      }
-      if (drawnEdges.has(recipe)) {
-        return;
-      }
-      drawnEdges.add(recipe);
-      const { x: x1, y: y1 } = coordinates.get(recipe)!;
-      for (const child of recipe.below) {
-        const { x: x2, y: y2 } = coordinates.get(child)!;
-        const line = document.createElementNS(ns, "line");
-        line.setAttribute("x1", (x1 + 50).toString());
-        line.setAttribute("y1", (y1 + 50).toString());
-        line.setAttribute("x2", (x2 + 50).toString());
-        line.setAttribute("y2", y2.toString());
-        line.setAttribute("stroke", "black");
-        line.setAttribute("stroke-width", "1");
-        edgeGroup.appendChild(line);
-        drawEdges(child);
-      }
-    };
 
     drawEdges(recipeDagRoot);
 

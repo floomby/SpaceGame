367de881488d4f4447472fc76805d867faad6381
Josh Hoover <caboodlennm@gmail.com>
2023-01-10 16:45:16 -0600

    Use SNI

--- server/settings.ts
+++ server/settings.ts
@@ -3,12 +3,27 @@ import { readFileSync } from "fs";
 import { useSsl } from "../src/config";
 
 const credentials: { key?: string; cert?: string; ca?: string } = {};
+const oldCredentials: { key?: string; cert?: string; ca?: string } = {};
 
 if (useSsl) {
   credentials.key = readFileSync("/etc/letsencrypt/live/spacequest.io/privkey.pem", "utf8");
   credentials.cert = readFileSync("/etc/letsencrypt/live/spacequest.io/cert.pem", "utf8");
   credentials.ca = readFileSync("/etc/letsencrypt/live/spacequest.io/chain.pem", "utf8");
+
+  oldCredentials.key = readFileSync("/etc/letsencrypt/live/inharmonious.floomby.us/privkey.pem", "utf8");
+  oldCredentials.cert = readFileSync("/etc/letsencrypt/live/inharmonious.floomby.us/cert.pem", "utf8");
+  oldCredentials.ca = readFileSync("/etc/letsencrypt/live/inharmonious.floomby.us/chain.pem", "utf8");
 }
+
+const sniCallback = (servername: string, cb: (err: Error | null, ctx: any) => void) => {
+  if (servername === "spacequest.io") {
+    cb(null, credentials);
+  } else if (servername === "inharmonious.floomby.us") {
+    cb(null, oldCredentials);
+  } else {
+    cb(new Error("Unknown servername"), null);
+  }
+};
 
 const wsPort = 8080;
 const httpPort = 8081;
@@ -24,4 +39,4 @@ const hash = (str: string) => {
 
 const adminHash = "77bf8af9b1f3f51c07556c529623732cb1feb23fae82b5fdaea68c7488362a91";
 
-export { credentials, wsPort, httpPort, hash, adminHash };
+export { sniCallback, wsPort, httpPort, hash, adminHash };

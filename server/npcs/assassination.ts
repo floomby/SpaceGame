32bf3fa3d53ea610da43b43e97bcb2f6e6ca9eb5
Josh Hoover <caboodlennm@gmail.com>
2023-01-11 18:21:08 -0600

    Improve assassination missions

--- server/npcs/assassination.ts
+++ server/npcs/assassination.ts
@@ -1,7 +1,7 @@
 import { assert } from "console";
 import { defMap, defs, emptyLoadout, emptySlotData, Faction, SlotKind } from "../../src/defs";
 import { estimateEffectivePrimaryRange, projectileDefs } from "../../src/defs/projectiles";
-import { effectiveInfinity, equip, findClosestTarget, GlobalState, Input, Player, randomNearbyPointInSector } from "../../src/game";
+import { CloakedState, effectiveInfinity, equip, findClosestTarget, GlobalState, Input, Player, randomNearbyPointInSector } from "../../src/game";
 import { l2Norm } from "../../src/geometry";
 import {
   idleState,
@@ -21,22 +21,30 @@ const makeStateGraph = (
   primaryRange: number,
   secondaryGuided: boolean,
   secondaryRange: number,
-  energyThreshold: number,
   mineSlot: null | number,
   impulseSlot: null | number
 ) => {
   const idle = idleState();
   const passiveGoTo = passiveGoToRandomPointInSector();
-  const swarm = stupidSwarmCombat(primaryRange, secondaryGuided, secondaryRange, energyThreshold, mineSlot);
-  const run = runAway(primaryRange, secondaryGuided, secondaryRange, energyThreshold, mineSlot);
-  const randomManeuver = randomCombatManeuver(primaryRange, secondaryGuided, secondaryRange, energyThreshold, mineSlot);
+  const swarm = stupidSwarmCombat(primaryRange, secondaryGuided, secondaryRange, 30, mineSlot);
+  const run = runAway(primaryRange, secondaryGuided, secondaryRange, 30, mineSlot);
+  const randomManeuver = randomCombatManeuver(primaryRange, secondaryGuided, secondaryRange, 30, mineSlot);
 
   // Total misuse of the transitions system to do actions (I should make a factory for making one frame fire states)
+  // Why not just break all the rules and get free ammo as well
   if (impulseSlot !== null) {
     swarm.transitions.push({
       trigger: (_, npc, ___, target) => {
-        if (Math.random() < 0.02 && !!target && l2Norm(npc.player.position, target.position) < 2500) {
+        if (Math.random() < 0.02 && !!target && l2Norm(npc.player.position, target.position) < 3800) {
           npc.secondariesToFire.push(impulseSlot);
+          for (const data of npc.player.slotData) {
+            if (data?.ammo === 0) {
+              data.ammo += 1;
+            }
+          }
+        }
+        if (npc.player.cloak === CloakedState.Uncloaked && npc.player.energy > 20) {
+          npc.player.cloak = 1;
         }
         return false;
       },
@@ -44,8 +52,16 @@ const makeStateGraph = (
     });
     run.transitions.push({
       trigger: (_, npc, ___, target) => {
-        if (Math.random() < 0.02 && !!target && l2Norm(npc.player.position, target.position) < 2500) {
+        if (Math.random() < 0.02 && !!target && l2Norm(npc.player.position, target.position) < 3800) {
           npc.secondariesToFire.push(impulseSlot);
+          for (const data of npc.player.slotData) {
+            if (data?.ammo === 0) {
+              data.ammo += 1;
+            }
+          }
+        }
+        if (npc.player.cloak === CloakedState.Uncloaked && npc.player.energy > 20) {
+          npc.player.cloak = 1;
         }
         return false;
       },
@@ -53,8 +69,16 @@ const makeStateGraph = (
     });
     randomManeuver.transitions.push({
       trigger: (_, npc, ___, target) => {
-        if (Math.random() < 0.02 && !!target && l2Norm(npc.player.position, target.position) < 2500) {
+        if (Math.random() < 0.02 && !!target && l2Norm(npc.player.position, target.position) < 3800) {
           npc.secondariesToFire.push(impulseSlot);
+          for (const data of npc.player.slotData) {
+            if (data?.ammo === 0) {
+              data.ammo += 1;
+            }
+          }
+        }
+        if (npc.player.cloak === CloakedState.Cloaked) {
+          npc.player.cloak = CloakedState.Uncloaked;
         }
         return false;
       },
@@ -144,6 +168,7 @@ class CloakyAnnoying implements NPC {
       v: { x: 0, y: 0 },
       iv: { x: 0, y: 0 },
       ir: 0,
+      cloak: 1,
     };
 
     // The striker does not have a mine slot, but I am leaving this check here anyways (I want this to work with other cloaky ships in the future)
@@ -169,12 +194,12 @@ class CloakyAnnoying implements NPC {
       case 0:
       case 1:
         this.player = equip(this.player, 1, "Javelin Missile", true);
-        this.currentState = makeStateGraph(estimateEffectivePrimaryRange(def), true, 3000, 3, mineSlot, impulseSlot);
+        this.currentState = makeStateGraph(estimateEffectivePrimaryRange(def), true, 3000, mineSlot, impulseSlot);
         break;
       case 2:
       case 3:
         this.player = equip(this.player, 1, "Tomahawk Missile", true);
-        this.currentState = makeStateGraph(estimateEffectivePrimaryRange(def), true, 2500, 3, mineSlot, impulseSlot);
+        this.currentState = makeStateGraph(estimateEffectivePrimaryRange(def), true, 2500, mineSlot, impulseSlot);
         break;
     }
 
